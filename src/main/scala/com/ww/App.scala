3fa6862acade43f3972fe3b14de6c1fdb281c6ed
ww530220264 <530220264@qq.com>
2020-06-27 23:12:50 +0800

    初始化

--- src/main/scala/com/ww/App.scala
+++ src/main/scala/com/ww/App.scala
@@ -1,7 +1,7 @@
 package com.ww
 
+import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.{SparkConf, SparkContext}
 
 /**
  * Hello world!
@@ -13,28 +13,25 @@ object App {
     val conf = new SparkConf()
       .setAppName("WordCount")
       // 设置yarn-client模式提交
-//      .setMaster("yarn")
+      .setMaster("yarn")
       // 设置resourcemanager的ip
-//      .set("yarn.resourcemanager.hostname", "cdh1")
+      .set("yarn.resourcemanager.hostname", "cdh1")
       // 设置executor的个数
-//      .set("spark.executor.instance", "2")
+      .set("spark.executor.instance", "2")
       // 设置executor的内存大小
-//      .set("spark.executor.memory", "512M")
+      .set("spark.executor.memory", "512M")
       // 设置提交任务的yarn队列
-//      .set("spark.yarn.queue", "spark")
+      .set("spark.yarn.queue", "spark")
       // 设置driver的ip地址
-//      .set("spark.driver.host", "192.168.10.106")
+      .set("spark.driver.host", "192.168.10.102")
       // 设置jar包的路径,如果有其他的依赖包,可以在这里添加,逗号隔开
-//      .setJars(List("C:\\workspace\\cdh\\target\\original-cdh-1.0-SNAPSHOT.jar"
-//      ))
-    val spark = SparkSession.builder().config(conf).enableHiveSupport().getOrCreate()
+      .setJars(List("C:\\ww\\cdh\\target\\original-cdh-1.0-SNAPSHOT.jar"
+      ))
+    val spark = SparkSession.builder().config(conf).getOrCreate()
     val sc = spark.sparkContext
     val lines = sc.parallelize(List("aa,bb,cc,dd,ee,aa,dd,ff,gg,ss,ee,cc,ss"))
     val words = lines.flatMap(_.split(","))
     val res = words.map((_, 1)).reduceByKey(_ + _)
     System.err.println("+++++++++++++++++++++++++++++++++++" + res.collect().mkString(","))
-
-    spark.sql("select * from test").show()
-
   }
 }
